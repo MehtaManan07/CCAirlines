ac1e8f7456a8ffa5d23b32e5cea5ed1493cd7be8
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-12-26 22:37:54 +0530

    Final dashboard admin

--- front/src/Components/core/Chart.js
+++ front/src/Components/core/Chart.js
@@ -1,25 +1,31 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment'
 
 const Chart = ({ items }) => {
   const lineChart = (
     <Line
       data={{
         labels: items.map((item) =>
-          new Date(item.createdAt).toString().slice(4, 15)
+          moment(new Date(item.createdAt)).format('D-M-YY')
         ),
         datasets: [
           {
             data: items.map((item) => item.price),
-            label: "Revenue",
-            borderColor: "#3333ff",
+            label: 'Revenue',
+            borderColor: '#3333ff',
             fill: true,
           },
         ],
       }}
     />
   );
-  return <div className="">{lineChart}</div>;
+  return (
+    <div className='mr-2'>
+      <h2 className="row"> REVENUE </h2>
+      <div className="row">{lineChart}</div>
+    </div>
+  );
 };
 
 export default Chart;
